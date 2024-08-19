52a397fd3103b5aaa01e6617e3288d5b08bf0296
Eduardo <jocdwardo25@gmail.com>
2024-08-18 19:45:34 -0500

    add: courses component to Fluyez academy page

--- src/app/Fluyez-Academy/page.js
+++ src/app/Fluyez-Academy/page.js
@@ -1,61 +1,83 @@
 "use client"
 import Image from "next/image"
 import LogoFluyez from "/public/assets/images/Fluyez-logo.png"
-import DesktopManAcademy from "/public/assets/images/fluyez-man-academy-desktop.png" 
-import MobileManAcademy from "/public/assets/images/fluyez-man-academy-mobile.png" 
+import DesktopManAcademy from "/public/assets/images/fluyez-man-academy-desktop.png"
+import MobileManAcademy from "/public/assets/images/fluyez-man-academy-mobile.png"
 import MenuMobile from "/public/assets/icons/menu-icon-mobile.png"
 import Footer from "@/components/footer"
+import CoursesAcademy from "@/components/Fluyez-Academy/courses-academy"
 
 const Academy = () => {
-return(<>
-      {/* <!-- Navbar --> */}
-      <nav class="bg-white  my-5">
-        <div class="container mx-auto px-4">
-          <div class="flex items-center justify-between">
-            {/* <!-- Logo --> */}
-            <div class="flex items-center space-x-2">
-              <Image src={LogoFluyez} alt="Logo" class="h-8 w-auto" />
-              {/* <span class="text-xl font-bold text-blue-700">fluyez<span class="text-gray-900">academy.</span></span> */}
-            </div>
-            {/* <!-- Navigation Links --> */}
-            <div class="hidden md:flex items-center space-x-4">
-              <a href="#" class="text-gray-700 hover:text-blue-700">Cursos</a>
-              <a href="#" class="text-gray-700 hover:text-blue-700">Blog</a>
-              <a href="#" class="text-gray-700 hover:text-blue-700">Glosario</a>
-              <a href="#" class="text-gray-700 hover:text-blue-700">Perú</a>
-              <a href="#" class="text-gray-700 hover:text-blue-700">Nosotros</a>
-              <a href="#" class="text-gray-700 hover:text-blue-700">Productos</a>
-              <a href="#" class="text-gray-700 hover:text-blue-700">Contacto</a>
-            </div>
-            <div>
+  return (<>
+    {/* <!-- Navbar --> */}
+    <nav class="bg-white  my-5">
+      <div class="container mx-auto px-4">
+        <div class="flex items-center justify-between">
+          {/* <!-- Logo --> */}
+          <div class="flex items-center space-x-2">
+            <Image src={LogoFluyez} alt="Logo" class="h-8 w-auto" />
+            {/* <span class="text-xl font-bold text-blue-700">fluyez<span class="text-gray-900">academy.</span></span> */}
+          </div>
+          {/* <!-- Navigation Links --> */}
+          <div class="hidden md:flex items-center space-x-4">
+            <a href="#" class="text-gray-700 hover:text-blue-700">Cursos</a>
+            <a href="#" class="text-gray-700 hover:text-blue-700">Blog</a>
+            <a href="#" class="text-gray-700 hover:text-blue-700">Glosario</a>
+            <a href="#" class="text-gray-700 hover:text-blue-700">Perú</a>
+            <a href="#" class="text-gray-700 hover:text-blue-700">Nosotros</a>
+            <a href="#" class="text-gray-700 hover:text-blue-700">Productos</a>
+            <a href="#" class="text-gray-700 hover:text-blue-700">Contacto</a>
+          </div>
+          <div>
             <Image src={MenuMobile} alt="Logo" class="h-8 w-auto block md:hidden" />
-            </div>
           </div>
         </div>
-      </nav>
+      </div>
+    </nav>
 
 
-      {/* <!-- Hero Section --> */}
-      <section class="container mx-auto px-4 py-8 flex flex-col md:flex-row items-center">
-        {/* <!-- Text Content --> */}
-        <div class="md:w-1/2 mt-8 md:mt-0">
-        <h2 className="text-lg font-semibold text-blue-600 mb-2">¡Aprende sobre crypto<br/> con nosotros!</h2>
-          <h1 class="text-4xl md:text-5xl font-bold text-blue-900 mb-4">Cursos de Fluyez Academy</h1>
-          <button class="bg-blue-600 text-white px-6 py-3 rounded-full hover:bg-blue-700 hidden md:block">Empieza aquí</button>
+    {/* <!-- Hero Section --> */}
+    <section class="container mx-auto px-4 py-8 flex flex-col md:flex-row items-center">
+      {/* <!-- Text Content --> */}
+      <div class="md:w-1/2 mt-8 md:mt-0">
+        <h2 className="text-lg font-semibold text-blue-600 mb-2">¡Aprende sobre crypto<br /> con nosotros!</h2>
+        <h1 class="text-4xl md:text-5xl font-bold text-blue-900 mb-4">Cursos de Fluyez Academy</h1>
+        <button class="bg-blue-600 text-white px-6 py-3 rounded-full hover:bg-blue-700 hidden md:block">Empieza aquí</button>
+      </div>
+      {/* <!-- Image Content --> */}
+      <div class="md:w-1/2">
+        <Image src={DesktopManAcademy} alt="Criptomonedas" class="hidden md:block" />
+        <Image src={MobileManAcademy} alt="Criptomonedas" class="block md:hidden" />
+      </div>
+      <button class="bg-blue-600 text-white px-6 py-3 rounded-full hover:bg-blue-700 block md:hidden ">Empieza aquí</button>
+    </section>
+    <div className="container mx-auto px-4 py-4">
+
+
+      <div className="flex justify-between items-center my-8">
+        <input
+          type="text"
+          placeholder="Buscar cursos..."
+          className="border rounded-lg p-2 w-1/3"
+        />
+        <div className="flex items-center">
+          <p className="mr-4">Ordenar por</p>
+          <select className="border rounded-lg p-2">
+            <option>Todos los niveles</option>
+          </select>
         </div>
-        {/* <!-- Image Content --> */}
-        <div class="md:w-1/2">
-          <Image src={DesktopManAcademy} alt="Criptomonedas" class="hidden md:block" />
-          <Image src={MobileManAcademy} alt="Criptomonedas" class="block md:hidden" />
-        </div>
-        <button class="bg-blue-600 text-white px-6 py-3 rounded-full hover:bg-blue-700 block md:hidden ">Empieza aquí</button>
-      </section>
+      </div>
+    </div>
+    <CoursesAcademy />
+    <CoursesAcademy />
+    <CoursesAcademy />
+    <CoursesAcademy />
 
 
 
-      <Footer/>
-    </>
-    )
-  }
+    <Footer />
+  </>
+  )
+}
 
-  export default Academy
+export default Academy
