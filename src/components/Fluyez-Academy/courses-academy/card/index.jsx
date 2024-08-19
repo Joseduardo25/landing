ea1751f04c17e7f079e9a5c586e2d0dc7c53301b
Eduardo <jocdwardo25@gmail.com>
2024-08-18 19:43:44 -0500

    add: card component to courses academy

---
+++ src/components/Fluyez-Academy/courses-academy/card/index.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+import Image from 'next/image';
+
+const Card = ({ title, description, image }) => {
+  return (
+    <div className="bg-white rounded-lg shadow-md p-4">
+      <Image src={image} alt={title} className="w-full h-40 object-cover rounded-lg" width={258} height={200} />
+      <h3 className="text-xl font-bold mt-4">{title}</h3>
+      <p className="text-gray-600 mt-2">{description}</p>
+    </div>
+  );
+};
+
+export default Card;
