d58f274e9cd55435360aecfafa7aa99a7a2714e8
Eduardo <jocdwardo25@gmail.com>
2024-08-28 03:25:32 -0500

    ADD: destructuring to use new values from json data

--- src/components/Fluyez-Academy/courses-academy/index.jsx
+++ src/components/Fluyez-Academy/courses-academy/index.jsx
@@ -1,27 +1,29 @@
 "use client"
 import Card from './card';
-import courses from './data/data.json';
+// import courses from './data/data.json';
 
-export default function CoursesAcademy() {
+export default function CoursesAcademy({courses}) {
+  const { title, content, relatedCourses } = courses;
+
   return (
     <div className="container mx-auto px-4 py-4">
       
       <div className='flex'>
 
 
-      <section className="my-8">
-        <h2 className="text-2xl font-bold">Fundamentos básicos</h2>
+      <section className="my-8 max-w-md pr-16">
+        <h2 className="text-2xl font-bold">{title}</h2>
         <p className="text-gray-600 mt-2">
-          Lorem ipsum dolor sit amet consectetur. Id pharetra ut pellentesque risus pharetra platea nunc a. Eu enim mauris odio tortor eu pellentesque proin.
+          {content}
         </p>
         <a href="#" className="text-blue-500 mt-4 inline-block">Ver todos &rarr;</a>
       </section>
       <div className="grid gap-4 lg:grid-cols-3">
-        {courses.map((course) => (
+        {relatedCourses.map((course) => (
           <Card
             key={course.id}
             title={course.title}
-            description={course.description}
+            // description={course.description}
             image={course.image}
             />
           ))}
