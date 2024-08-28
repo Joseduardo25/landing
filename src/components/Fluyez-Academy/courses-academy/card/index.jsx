3ef4a8e2cad5406b73148743cc0fac7162a4d4af
Eduardo <jocdwardo25@gmail.com>
2024-08-28 03:26:53 -0500

    UPDATE: disable paragraph description and prop description

--- src/components/Fluyez-Academy/courses-academy/card/index.jsx
+++ src/components/Fluyez-Academy/courses-academy/card/index.jsx
@@ -2,12 +2,12 @@
 
 import Image from 'next/image';
 
-const Card = ({ title, description, image }) => {
+const Card = ({ title, image }) => {
   return (
     <div className="bg-white rounded-lg shadow-md p-4">
-      <Image src={image} alt={title} className="w-full h-40 object-cover rounded-lg" width={258} height={200} />
+      <Image src={image} alt={title} className="w-full h-52 object-cover rounded-lg" width={258} height={200} />
       <h3 className="text-xl font-bold mt-4">{title}</h3>
-      <p className="text-gray-600 mt-2">{description}</p>
+      {/* <p className="text-gray-600 mt-2">{description}</p> */}
     </div>
   );
 };
